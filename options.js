5579db20b652f3fe19d57bf02f787889f47c2786
Brett Doyle <brettd3000@gmail.com>
2019-06-13 20:29:09 +0100

    Added options page

---
+++ options.js
@@ -0,0 +1,34 @@
+let inputs = document.querySelectorAll('input')
+
+function onCheckboxChange() {
+    let obj = {}
+    obj[this.id] = this.checked
+
+    chrome.storage.sync.set(obj)
+}
+function onInputChange() {
+    let obj = {}
+    obj[this.id] = this.value
+
+    chrome.storage.sync.set(obj)
+}
+
+for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].type === 'checkbox') {
+        inputs[i].addEventListener('change', onCheckboxChange)
+    } else {
+        inputs[i].addEventListener('input', onInputChange)
+    }
+
+    chrome.storage.sync.get(inputs[i].id, result => {
+        if (inputs[i].type === 'checkbox') {
+            if (typeof result[inputs[i].id] === 'boolean') {
+                inputs[i].checked = result[inputs[i].id]
+            }
+        } else {
+            if (result[inputs[i].id]) {
+                inputs[i].value = result[inputs[i].id]
+            }
+        }
+    })
+}
